11e6b4eb7b9a2328af8ac73782c76246f5d0fc45
DavidGL17 <david.gonzalez@citycable.ch>
2023-03-10 10:34:01 +0100

    fin lab01

--- lab01/scrappy/crawler/spiders/wiki_spider.py
+++ lab01/scrappy/crawler/spiders/wiki_spider.py
@@ -18,6 +18,20 @@ class WikiSpider(scrapy.Spider):
         super(WikiSpider, self).__init__(*args, **kwargs)
         if self.es.indices.exists(index=self.es_index):
             self.es.indices.delete(index=self.es_index)
+        # create index
+        self.es.indices.create(index=self.es_index)
+        # explain the data
+        self.es.indices.put_mapping(
+            index=self.es_index,
+            body={
+                "properties": {
+                    "url": {"type": "text"},
+                    "name": {"type": "text"},
+                    "external_links": {"type": "integer"},
+                    "headline": {"type": "text"},
+                }
+            },
+        )
 
     def parse(self, response):
         # Check if current URL is a subpage of fr.wikipedia.org
@@ -27,16 +41,37 @@ class WikiSpider(scrapy.Spider):
             ).get()
             json_data = json.loads(schema_org)
 
+            external_links = 0
+            # Find all links to subpages of fr.wikipedia.org
+            for href in response.css("a::attr(href)").getall():
+                absolute_url = urljoin(response.url, href)
+                if self.allowed_domain in absolute_url:
+                    external_links += 1
+                    yield response.follow(href, self.parse)
+
+            headline = ""
+            if "headline" in json_data:
+                headline = json_data["headline"]
+
+            p_tags = response.css("div.mw-parser-output > p")
+            first_paragraph_text = ""
+            for p_tag in p_tags:
+                # if tag has class attribute and if tag contains class "mv-empty-elt", it is an empty tag, so we skip it
+                if p_tag.attrib.get("class") and "mw-empty-elt" in p_tag.attrib.get(
+                    "class"
+                ):
+                    continue
+                else:
+                    first_paragraph_text = "".join(p_tag.css("::text").getall())
+                    break
+
             # save the url and name in the json data in a separate dictionary
             final_dict = {
                 "url": json_data["url"],
                 "name": json_data["name"],
+                "external_links": external_links,
+                "headline": headline,
+                "first_paragraph": first_paragraph_text,
             }
 
             self.es.index(index=self.es_index, body=final_dict)
-
-        # Find all links to subpages of fr.wikipedia.org
-        for href in response.css("a::attr(href)").getall():
-            absolute_url = urljoin(response.url, href)
-            if self.allowed_domain in absolute_url:
-                yield response.follow(href, self.parse)
